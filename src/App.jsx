c10ba43d640cd8be6b3cf4b01aac161d1674f106
Minh Đinh <minhdq2512@gmail.com>
2024-07-16 19:06:11 +0700

    Add more API and UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Product from "./pages/product/Product";
 import Payment from "./pages/product/Payment";
 import Create from "./pages/product/Create";
@@ -22,33 +17,34 @@ import EventDetails from "./components/eventDetails/EventDetails";
 import { CartProvider } from "./pages/product/CartContext";
 import DashboardVendor from "./pages/VendorDashboard/VendorDashboard";
 import SalesShopOrder from "./pages/product/Product";
+import MobileDetector from "./MobileDetector";
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Routes>
-          {/* <Route path="/" element={<Navigate to="/owner" />} />
-          <Route path="/owner" element={<OwnerPage />} /> */}
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/:shopId/products" element={<SalesShopOrder />} />
-          <Route path="/ListAccounts" element={<ListAccounts />} />
-          <Route path="/ListCards" element={<ListCards />} />
-          <Route path="/ListOrders" element={<ListOrders />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/VendorDash" element={<DashboardVendor />} />
-          <Route path="/Product" element={<Product />} />
-          <Route path="/Payment" element={<Payment />} />
-          <Route path="/Create" element={<Create />} />
-          <Route path="/Cart" element={<Cart />} />
-          <Route path="/ListOrdered" element={<ListOrdered />} />
-          <Route path="/event/:id" element={<EventDetails />} />
-        </Routes>
-      </Router>
-    </CartProvider>
+    <MobileDetector>
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Homepage />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/shop/:shopId/products" element={<SalesShopOrder />} />
+            <Route path="/ListAccounts" element={<ListAccounts />} />
+            <Route path="/ListCards" element={<ListCards />} />
+            <Route path="/ListOrders" element={<ListOrders />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/VendorDash" element={<DashboardVendor />} />
+            <Route path="/Product" element={<Product />} />
+            <Route path="/Payment" element={<Payment />} />
+            <Route path="/Create" element={<Create />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="/ListOrdered" element={<ListOrdered />} />
+            <Route path="/event/:id" element={<EventDetails />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </MobileDetector>
   );
 }
 
